ba4145e821499613c2dfc9daa8bba0662b11084d
jhearvisc <82467631+jhearvisc@users.noreply.github.com>
2025-01-09 00:48:13 -0600

    device_info_plus replaced

--- platform_device_id/lib/platform_device_id.dart
+++ platform_device_id/lib/platform_device_id.dart
@@ -1,15 +1,12 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/services.dart';
-import 'package:device_info_plus/device_info_plus.dart';
+import 'package:android_id/android_id.dart';
 import 'package:platform_device_id_platform_interface/platform_device_id_platform_interface.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 /// Provides device id information.
 class PlatformDeviceId {
-  /// Provides device and operating system information.
-  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
-
   /// Information derived from `android`-`androidId` or `ios`-`identifierForVendor`
   static Future<String?> get getDeviceId async {
     String? deviceId;
@@ -17,11 +14,9 @@ class PlatformDeviceId {
       if (kIsWeb) {
         deviceId = await PlatformDeviceIdPlatform.instance.getDeviceId();
       } else if (Platform.isAndroid) {
-        AndroidDeviceInfo androidInfo = await deviceInfoPlugin.androidInfo;
-        deviceId = androidInfo.androidId;
+        deviceId = await _androidIdPlugin.getId();
       } else if (Platform.isIOS) {
-        IosDeviceInfo iosInfo = await deviceInfoPlugin.iosInfo;
-        deviceId = iosInfo.identifierForVendor;
+        deviceId = await _androidIdPlugin.getId();
       } else {
         deviceId = await PlatformDeviceIdPlatform.instance.getDeviceId();
       }
